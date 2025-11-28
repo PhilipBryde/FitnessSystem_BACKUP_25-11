dd5a52ec7cb3ad20e0d52efdaf6e6101cc007041
cpu-man <71815606+cpu-man@users.noreply.github.com>
2025-11-28 12:12:14 +0100

    Remove Member Added

--- FitnessProgram/MemberWindow.xaml.cs
+++ FitnessProgram/MemberWindow.xaml.cs
@@ -36,18 +36,47 @@ namespace FitnessProgram
             //string allMembers = "";
             for (int i = 0; i < localList.Count; i++) //For løkke der printer alle medlemmer ud
             {
-                var member = localList[i];
+                Member member = localList[i];
                 allMembers.AppendLine($"ID: {member.id} Navn: {member.name} Køn: {member.gender}"); //printer hvert variabel i hver sin linje, da StringBuilder gør det til én stor string
                 
             }
             MemberBlock.Text = allMembers.ToString(); //Laver vores StringBuilder om til den endelige string og ligger det ind i vores TextBlock
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void RemoveMember()
+        {
+            if (int.TryParse(EnterMember.Text, out int memberID)) // Brugers input bliver konverteret til en int vi kalder for memberID
+            {
+                Member member = fitness.GetAllMembers().FirstOrDefault(member => member.id == memberID); //Finder et matchende ID; hvis der ikke bliver fundet noget får vi 'null'
+                if (member != null) //Hvis den får et matchene id, kører denne del
+                {
+                    fitness.GetAllMembers().Remove(member); //Fjerner medlemmen
+                    MessageBox.Show($"{memberID} slettet!"); //Besked til brugeren
+                    ShowMembers(); //Genindlæser listen
+                }
+                else
+                {
+                    MessageBox.Show($"{memberID} ikke fundet, prøv igen"); //Hvis ID'et ikke matcher vises denne besked
+                }
+            }
+            else
+            {
+                MessageBox.Show("Indtast venligst et tal"); //Hvis der ikke bliver tastet et tal ind, vises denne besked
+            }
+            
+        }
+
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             NextWindow next = new NextWindow();
             next.Show();
             this.Close();
+        }
+
+        private void DeleteMemberButton_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveMember();
         }
     }
 }
